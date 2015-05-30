d9c26112904012df38379ca421acbe826adafaed
Shimon Magal <shimon.magal@gmail.com>
2015-05-31 01:31:09 +0300

    decide new API

---
+++ test/src/main/java/oracle2mongo/Oracle2Mongo.java
@@ -0,0 +1,19 @@
+package oracle2mongo;
+
+import java.io.File;
+
+public class Oracle2Mongo {
+	
+	public Oracle2Mongo(String jdbcUrl, String mongoUrl, String mongoDBName, File configuration){
+		
+	}
+	
+	public Oracle2Mongo(String jdbcUrl, String mongoUrl, String mongoDBName){
+		
+	}
+	
+	private Oracle2Mongo(String jdbcUrl, String mongoUrl, String mongoDBName, String configurationString){
+		
+	}
+
+}
